118fae0d053c9a5204f98b3cb65c028fb2962594
Marius Hamacher <marius.hamacher@stud.uni-due.de>
2017-09-29 17:40:11 +0200

    Implement path Stat Display & Calculation

--- planner/careerWidget.js
+++ planner/careerWidget.js
@@ -1,4 +1,4 @@
-var nodes = [{"id":0,"label":"Prodigy","group":"Basic","color":"#2E86AB","career":"Prodigy","type":"Basic","role":"Academics"},{"id":1,"label":"Dilletante","group":"Basic","color":"#EDDFCE","career":"Dilletante","type":"Basic","role":"Commoners"},{"id":2,"label":"Student","group":"Basic","color":"#2E86AB","career":"Student","type":"Basic","role":"Academics"},{"id":3,"label":"Apprentice Wizard","group":"Basic","color":"#2E86AB","career":"Apprentice Wizard","type":"Basic","role":"Academics"},{"id":4,"label":"Wizard","group":"Advanced","color":"#2E86AB","career":"Wizard","type":"Advanced","role":"Academics"},{"id":5,"label":"Wizard Lord","group":"Advanced","color":"#2E86AB","career":"Wizard Lord","type":"Advanced","role":"Academics"},{"id":6,"label":"Scholar","group":"Advanced","color":"#2E86AB","career":"Scholar","type":"Advanced","role":"Academics"},{"id":7,"label":"Explorer","group":"Advanced","color":"#C5D86D","career":"Explorer","type":"Advanced","role":"Rangers"},{"id":8,"label":"Herald","group":"Advanced","color":"#EDDFCE","career":"Herald","type":"Advanced","role":"Commoners"},{"id":9,"label":"Messenger","group":"Basic","color":"#C5D86D","career":"Messenger","type":"Basic","role":"Rangers"},{"id":10,"label":"Militiaman","group":"Basic","color":"#FE7F2D","career":"Militiaman","type":"Basic","role":"Warriors"}]
+var nodes = [{"career":"Prodigy","type":"Basic","role":"Academics","exits":"Student","weapon skill":0.0,"ballistic skill":0.0,"strength":0.0,"toughness":0.0,"agility":5.0,"intelligence":15.0,"will power":10.0,"fellowship":15.0,"attacks":0.0,"wounds":0.0,"magic":0.0,"id":0,"label":"Prodigy","group":"Basic","color":"#2E86AB"},{"career":"Dilletante","type":"Basic","role":"Commoners","exits":"Student","weapon skill":5.0,"ballistic skill":5.0,"strength":0.0,"toughness":0.0,"agility":5.0,"intelligence":5.0,"will power":5.0,"fellowship":10.0,"attacks":0.0,"wounds":1.0,"magic":0.0,"id":1,"label":"Dilletante","group":"Basic","color":"#EDDFCE"},{"career":"Student","type":"Basic","role":"Academics","exits":"Scholar","weapon skill":0.0,"ballistic skill":0.0,"strength":0.0,"toughness":0.0,"agility":10.0,"intelligence":10.0,"will power":5.0,"fellowship":10.0,"attacks":0.0,"wounds":2.0,"magic":0.0,"id":2,"label":"Student","group":"Basic","color":"#2E86AB"},{"career":"Apprentice Wizard","type":"Basic","role":"Academics","exits":"Scholar","weapon skill":0.0,"ballistic skill":0.0,"strength":0.0,"toughness":0.0,"agility":5.0,"intelligence":10.0,"will power":15.0,"fellowship":5.0,"attacks":0.0,"wounds":2.0,"magic":1.0,"id":3,"label":"Apprentice Wizard","group":"Basic","color":"#2E86AB"},{"career":"Wizard","type":"Advanced","role":"Academics","exits":"Wizard Lord, Scholar, Explorer","weapon skill":10.0,"ballistic skill":10.0,"strength":0.0,"toughness":10.0,"agility":15.0,"intelligence":30.0,"will power":35.0,"fellowship":15.0,"attacks":0.0,"wounds":4.0,"magic":3.0,"id":4,"label":"Wizard","group":"Advanced","color":"#2E86AB"},{"career":"Wizard Lord","type":"Advanced","role":"Academics","exits":"Explorer","weapon skill":15.0,"ballistic skill":15.0,"strength":5.0,"toughness":15.0,"agility":20.0,"intelligence":35.0,"will power":40.0,"fellowship":20.0,"attacks":0.0,"wounds":5.0,"magic":4.0,"id":5,"label":"Wizard Lord","group":"Advanced","color":"#2E86AB"},{"career":"Scholar","type":"Advanced","role":"Academics","exits":"Wizard, Explorer","weapon skill":null,"ballistic skill":null,"strength":null,"toughness":null,"agility":null,"intelligence":null,"will power":null,"fellowship":null,"attacks":null,"wounds":null,"magic":null,"id":6,"label":"Scholar","group":"Advanced","color":"#2E86AB"},{"career":"Explorer","type":"Advanced","role":"Rangers","exits":" ","weapon skill":20.0,"ballistic skill":20.0,"strength":10.0,"toughness":15.0,"agility":15.0,"intelligence":25.0,"will power":20.0,"fellowship":15.0,"attacks":1.0,"wounds":6.0,"magic":0.0,"id":7,"label":"Explorer","group":"Advanced","color":"#C5D86D"},{"career":"Herald","type":"Advanced","role":"Commoners","exits":"Explorer","weapon skill":10.0,"ballistic skill":10.0,"strength":5.0,"toughness":5.0,"agility":15.0,"intelligence":15.0,"will power":10.0,"fellowship":20.0,"attacks":0.0,"wounds":4.0,"magic":0.0,"id":8,"label":"Herald","group":"Advanced","color":"#EDDFCE"},{"career":"Messenger","type":"Basic","role":"Rangers","exits":"Herald, Explorer","weapon skill":5.0,"ballistic skill":5.0,"strength":0.0,"toughness":5.0,"agility":10.0,"intelligence":5.0,"will power":5.0,"fellowship":0.0,"attacks":0.0,"wounds":2.0,"magic":0.0,"id":9,"label":"Messenger","group":"Basic","color":"#C5D86D"},{"career":"Militiaman","type":"Basic","role":"Warriors","exits":"Messenger","weapon skill":10.0,"ballistic skill":5.0,"strength":5.0,"toughness":5.0,"agility":10.0,"intelligence":0.0,"will power":0.0,"fellowship":0.0,"attacks":0.0,"wounds":2.0,"magic":0.0,"id":10,"label":"Militiaman","group":"Basic","color":"#FE7F2D"}]
 
 var edges = [{"from":1,"to":2,"weight":1,"id":"1--2"},{"from":0,"to":2,"weight":1,"id":"0--2"},{"from":5,"to":7,"weight":1,"id":"5--7"},{"from":4,"to":7,"weight":1,"id":"4--7"},{"from":8,"to":7,"weight":1,"id":"8--7"},{"from":10,"to":9,"weight":1,"id":"10--9"},{"from":6,"to":7,"weight":1,"id":"6--7"},{"from":4,"to":5,"weight":1,"id":"4--5"},{"from":9,"to":7,"weight":1,"id":"9--7"},{"from":3,"to":6,"weight":1,"id":"3--6"},{"from":9,"to":8,"weight":1,"id":"9--8"},{"from":6,"to":4,"weight":1,"id":"6--4"},{"from":2,"to":6,"weight":1,"id":"2--6"},{"from":4,"to":6,"weight":1,"id":"4--6"}]
 
@@ -58,7 +58,8 @@ var options = {
     }
 }
 var container = document.getElementById('widget')
-var pathdisplay = document.getElementById('pathdisplay')
+var pathDisplay = document.getElementById('path-display')
+var statDisplay = document.getElementById('stat-display')
 var selectedNodes = []
 
 var network = new vis.Network(container, dataset, options)
@@ -94,6 +95,24 @@ nodes.forEach(node => careerIndex[node.label] = node.id)
 var graph = new GraphFactory().createDirectedGraph(careers, edges)
 var searcher = new GraphSearcher(graph)
 
+function maxAdvances(selection) {
+    var stats = ['weapon skill', 'ballistic skill', 'strength', 'toughness', 'agility', 'intelligence', 'will power', 'fellowship', 'attacks', 'wounds', 'magic']
+    var combinedStats = {}
+    stats.forEach(stat => {
+        var values = selectedNodes.map(x => dataset.nodes.get(x)[stat])
+        combinedStats[stat] = values.reduce((a,b) => Math.max(a,b))
+    })
+    return combinedStats
+}
+
+function updateStatDisplay(stats) {
+    var display = 'You need to level the following Stats: '
+    for (stat in stats) {
+        display += stat + ':' + stats[stat] + ' '
+    }
+    statDisplay.innerText = display
+}
+
 function findPath(start, target) {
     var pathStrings = {
         path: 'The fastest path for your selection is: ',
@@ -104,9 +123,10 @@ function findPath(start, target) {
     network.fit({nodes: path, animation: true})
     if (path.length > 0) {
         var pathCareers = path.map(id => careers[id])
-        pathdisplay.innerText = pathStrings.path + pathCareers
+        pathDisplay.innerText = pathStrings.path + pathCareers
+        updateStatDisplay(maxAdvances(path))
     } else {
-        pathdisplay.innerText = pathStrings.nopath
+        pathDisplay.innerText = pathStrings.nopath
         selectedNodes.pop()
         network.unselectAll()
         network.selectNodes(selectedNodes, true)
